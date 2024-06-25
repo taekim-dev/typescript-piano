94d4c9f8b282b2ebcb31c00342a14aac10a2083a
Tae Kim <taekim.dev@gmail.com>
2024-06-25 21:04:57 +0800

    migrate audio store

---
+++ store/audio.ts
@@ -0,0 +1,42 @@
+import { defineStore } from 'pinia'
+
+interface Audio {
+  title: string;
+  input: any;
+  keys: any;
+}
+
+interface AudioState {
+  savedAudios: Audio[];
+  playingAudios: any[];
+  SAVE_LIMIT: number;
+}
+
+export const useAudioStore = defineStore<'audio', AudioState>({
+  id: 'audio',
+  state: (): AudioState => ({
+    savedAudios: [],
+    playingAudios: [],
+    SAVE_LIMIT: 10,
+  }),
+
+  actions: {
+    saveAudio(title: string, input: any, keys: any): void {
+      if (this.savedAudios.length >= this.SAVE_LIMIT) {
+        this.savedAudios.shift()
+      }
+      this.savedAudios.push({ title, input, keys })
+      localStorage.setItem('savedAudios', JSON.stringify(this.savedAudios))
+    },
+    clearAudios(): void {
+      this.savedAudios = []
+      localStorage.removeItem('savedAudios')
+    },
+    loadSavedAudios(): void {
+      const savedAudiosFromStorage = localStorage.getItem('savedAudios')
+      if (savedAudiosFromStorage) {
+        this.savedAudios = JSON.parse(savedAudiosFromStorage)
+      }
+    },
+  },
+})
