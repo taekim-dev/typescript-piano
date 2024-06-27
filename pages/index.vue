889acfe328d46daec3475a26697d8d6317c0a534
Tae Kim <taekim.dev@gmail.com>
2024-06-28 07:42:24 +0800

    remove store boilerplate

--- pages/index.vue
+++ pages/index.vue
@@ -7,8 +7,6 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { useAudioStore } from '~/store/audio'
-import { useKeyStore } from '~/store/key'
 import PianoUIComponent from '~/components/PianoUIComponent.vue'
 import InputComponent from '~/components/InputComponent.vue'
 
@@ -17,16 +15,6 @@ export default defineComponent({
   components: {
     PianoUIComponent,
     InputComponent,
-  },
-  setup() {
-    const audioStore = useAudioStore()
-    const keyStore = useKeyStore()
-
-    console.log(audioStore, keyStore)
-    return {
-      audioStore,
-      keyStore,
-    }
   },
 })
 </script>
